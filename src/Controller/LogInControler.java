c6eb0af23a159d321414e4514fddd44896383739
Vinícius Ventura <vini.ventura999@gmail.com>
2022-11-09 18:26:13 -0300

    working on logIn controller

--- src/Controller/LogInControler.java
+++ src/Controller/LogInControler.java
@@ -1,5 +1,26 @@
 package Controller;
 
-public class LogInControler {
-    
+import Model.DAO.AccountDAO;
+import Model.Entity.Account;
+
+public class LogInControler implements ILogInController<Account> {
+
+    private AccountDAO accountDAO;
+
+    public LogInControler() {
+        super();
+        try {
+            accountDAO = new AccountDAO();
+            System.out.println("DAO successfully built.");
+        } catch (Exception e) {
+            System.out.println("Problem managing the connection to the data source");
+        }
+    }
+
+    @Override
+    public boolean logIn(String username, String password) {
+        // TODO Auto-generated method stub
+        return false;
+    }
+
 }
