3943449a7c1d19db71b5a151cbcafe26857e22f4
Vinícius Ventura <vini.ventura999@gmail.com>
2022-11-10 16:23:16 -0300

    formatting

--- src/Model/DAO/AccountDAO.java
+++ src/Model/DAO/AccountDAO.java
@@ -4,18 +4,16 @@ import java.sql.Connection;
 import java.sql.Statement;
 import java.sql.ResultSet;
 
-
 import Model.Entity.Account;
-
 
 public class AccountDAO extends GenericDAO<Account> {
 
     private Connection con = null;
-	private Statement stmt = null;
-	private Account answer = null;
-	private ResultSet result = null;
-    
+    private Statement stmt = null;
+    private Account answer = null;
+    private ResultSet result = null;
+
     public boolean logIn(Account account) {
-      return false;    
+        return false;
     }
 }
