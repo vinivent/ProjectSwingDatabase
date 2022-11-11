274e4ff669f51c1119c97daae24529d95f26b445
Vinícius Ventura <vini.ventura999@gmail.com>
2022-11-11 19:18:48 -0300

    implementing main (version beta)

--- src/View/StoreMain.java
+++ src/View/StoreMain.java
@@ -10,14 +10,8 @@ public class StoreMain {
             e.printStackTrace();
         }
         try {
-            // ManageProduct manageProduct = new ManageProduct();
-            // manageProduct.setVisible(true);
-            Operations operations = new Operations();
-            operations.setVisible(true);
-            // RegisterProduct registerFrame = new RegisterProduct();
-            // registerFrame.setVisible(true);
-            // LogIn frame = new LogIn();
-            // frame.setVisible(true);
+            LogIn frame = new LogIn();
+            frame.setVisible(true);
         } catch (Exception e) {
             e.printStackTrace();
         }
