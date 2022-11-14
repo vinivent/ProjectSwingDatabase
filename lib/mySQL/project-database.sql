96dad6978c2cab7d196f45e25b40313ee8696e8e
Vinícius Ventura <vini.ventura999@gmail.com>
2022-11-14 19:25:33 -0300

    database uploaded into project

---
+++ lib/mySQL/project-database.sql
@@ -0,0 +1,10 @@
+create table registerProduct (
+	id varchar(30) primary key,
+    productAmount varchar(30),
+    productName varchar(250),
+    productCategory varchar(30),
+    purchaseValue varchar(30),
+	saleValue varchar(30),
+    productRegisterDate date
+
+);
